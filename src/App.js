59037e7854c5aa3cfb8afbf7b905638785c21c43
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-03-02 15:56:39 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { home } from "./pages/home";
 import "./App.css";
+
+//pages
+import { home } from "./pages/home";
+import { signup } from "./pages/signup";
+import { login } from "./pages/login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={home} />
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@material-ui/core/Appbar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <AppBar>
+      <Toolbar>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Log in</Button>
+        <Button color="inherit">Sign up</Button>
+        <Button color="inherit">Find a listing</Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;

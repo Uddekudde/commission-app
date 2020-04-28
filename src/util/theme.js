362793470892da33735a9be924d1ce7e124e071a
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-04-28 13:48:02 +0200

    added horizontal rule to theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -1,34 +1,37 @@
 export default {
   palette: {
     primary: {
-      main: "#ffffff"
+      main: "#ffffff",
     },
     secondary: {
-      main: "#ff4081"
-    }
+      main: "#ff4081",
+    },
   },
   spreadThis: {
     formContainer: {
       textAlign: "center",
       width: "400px",
-      margin: "0 auto 0 auto"
+      margin: "0 auto 0 auto",
     },
     title: {
-      color: "#484848"
+      color: "#484848",
     },
     textField: {
-      margin: "10px 0 10px 0"
+      margin: "10px 0 10px 0",
     },
     button: {
-      marginTop: "20px"
+      marginTop: "20px",
     },
     signup: {
       marginTop: "20px",
-      color: "black"
+      color: "black",
     },
     errorMessage: {
       color: "red",
-      marginTop: "10px"
-    }
-  }
+      marginTop: "10px",
+    },
+    hr: {
+      border: "0.5px solid #dddddd",
+    },
+  },
 };

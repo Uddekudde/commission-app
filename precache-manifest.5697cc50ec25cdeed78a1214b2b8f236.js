b7e6d680d79ea9020e3b2bbf8058640e90fb680b
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2020-05-12 16:07:28 +0200

    Updates

---
+++ precache-manifest.5697cc50ec25cdeed78a1214b2b8f236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9d19a0ebbcf98cc457ff708ddbc903",
+    "url": "/commission-app/index.html"
+  },
+  {
+    "revision": "e47c91365812854a6d41",
+    "url": "/commission-app/static/css/main.5d640927.chunk.css"
+  },
+  {
+    "revision": "e5f23b418742d86b7d66",
+    "url": "/commission-app/static/js/2.8e2fa206.chunk.js"
+  },
+  {
+    "revision": "432ed034d9831dc13cf75201c058fd1d",
+    "url": "/commission-app/static/js/2.8e2fa206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47c91365812854a6d41",
+    "url": "/commission-app/static/js/main.46ea68d9.chunk.js"
+  },
+  {
+    "revision": "3dc0950294df5bfe8351",
+    "url": "/commission-app/static/js/runtime-main.92fa8963.js"
+  }
+]);
